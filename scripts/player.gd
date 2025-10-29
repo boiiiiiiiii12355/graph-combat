d60639750be4ccdbbeaa7fc5d86252cd4b014eff
Mel_The_Idiot <mel_the_idiot@Cachy>
2025-10-29 13:08:09 +0800

    Update game.tscn, math_camera.tscn, project.godot, and 428 more files

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,12 +2,26 @@ extends CharacterBody2D
 
 @export var ruler_x : Line2D
 @export var ruler_y : Line2D
-@export var ruler_color = Color(0.282, 0.282, 0.282)
+@export var ruler_x_color = Color(0.788, 0.0, 0.0, 1.0)
+@export var ruler_y_color = Color(0.0, 0.672, 0.0, 1.0)
 @export var ruler_width = 2
+
 func _ready() -> void:
 	ruler_x.add_point(Vector2(-1000, 0))
 	ruler_y.add_point(Vector2(0, -1000))
-	ruler_x.default_color = ruler_color
-	ruler_y.default_color = ruler_color
+	ruler_x.default_color = ruler_x_color
+	ruler_y.default_color = ruler_y_color
 	ruler_x.width = ruler_width
 	ruler_y.width = ruler_width
+
+func _process(delta: float) -> void:
+	move_and_slide()
+	
+func _physics_process(delta: float) -> void:
+	movement()
+	
+@export var gravity = 9.3
+func movement():
+	if not is_on_floor():
+		velocity.y += gravity
+		
