19e89653ecf9156933aa3df2f732e657bac98939
Mel_The_Idiot <mel_the_idiot@Cachy>
2025-10-29 12:23:18 +0800

    Update game.tscn, player.tscn, ui.tscn, and 1 more file

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,1 +1,13 @@
 extends CharacterBody2D
+
+@export var ruler_x : Line2D
+@export var ruler_y : Line2D
+@export var ruler_color = Color(0.282, 0.282, 0.282)
+@export var ruler_width = 2
+func _ready() -> void:
+	ruler_x.add_point(Vector2(-1000, 0))
+	ruler_y.add_point(Vector2(0, -1000))
+	ruler_x.default_color = ruler_color
+	ruler_y.default_color = ruler_color
+	ruler_x.width = ruler_width
+	ruler_y.width = ruler_width
